291f320ffc13c4c5155b6dc564dcb03b33c08743
Bojan <bojanj6m@gmail.com>
2017-03-10 17:02:18 -0500

    Added gmail settings, commented for now

--- ideas/settings/production.py
+++ ideas/settings/production.py
@@ -27,6 +27,17 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['mentideas3.herokuapp.com']
 
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'andresmeona@gmail.com' #my gmail username
+# EMAIL_HOST_PASSWORD = 'dulepacov88' #my gmail password
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = "Justin <hungrypy@gmail.com>"
+
+
+ADMINS = [('Justin', EMAIL_HOST_USER)]
+MANAGERS = ADMINS
+
 
 # Application definition
 
