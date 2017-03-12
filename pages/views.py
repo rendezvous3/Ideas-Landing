901d506e468876615aed0bd285cd81528d3e54f8
Bojan <bojanj6m@gmail.com>
2017-03-11 22:33:37 -0500

    Page model and dynamic homepage

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View, FormView, CreateView
 
 # Create your views here.
 
+from .models import Page
 from newsletter.forms import JoinForm
 
 # class HomeView(View):
@@ -16,6 +17,11 @@ class HomeView(SuccessMessageMixin, CreateView):
 	template_name = 'pages/home.html'
 	form_class    = JoinForm
 	success_url = "/"
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomeView, self).get_context_data(*args, **kwargs)
+		context['page_obj'] = Page.objects.all().first() #.order_by("?").first()
+		return context
 
 	def get_success_message(self, cleaned_data):
 		print(cleaned_data)
