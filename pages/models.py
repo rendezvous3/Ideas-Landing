e8e4fa503ec304d9aa63c1b03f41f8eddb0c7ebe
Bojan <bojanj6m@gmail.com>
2017-03-13 18:18:55 -0400

    Pages format & Layout

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -7,6 +8,13 @@ LAYOUT_CHOICES = (
 		('stacked', 'Stacked'),
 	)
 
+def layout_validator(value):
+	if value[0] != "#":
+		raise ValidationError("Must start with #")
+	if len(value) == 4 or len(value) == 7:
+		return value
+	raise ValidationError("Incorrect length")		
+
 class Page(models.Model):
 	title             = models.CharField(max_length=220)
 	title_description = models.TextField(blank=True, null=True)
@@ -14,7 +22,7 @@ class Page(models.Model):
 	title_btn_url     = models.CharField(max_length=50, blank=True, null=True)
 	content           = models.TextField(blank=True, null=True)
 	show_nav          = models.BooleanField(default=True)
-	nav_color         = models.CharField(max_length=7, default="#000000")
+	nav_color         = models.CharField(max_length=7, default="#000000", validators=[])
 	layout            = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default='standard')
 	video_embed       = models.TextField(null=True, blank=True)
 
